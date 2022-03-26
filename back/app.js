7ea04c3fb5b1996ab5f28695b008efea8198f0d5
Daniel <Daniel Santiago>
2022-03-26 14:48:38 +0100

    Added register endpoint

--- back/app.js
+++ back/app.js
@@ -1,15 +1,38 @@
 const SECRET = "randomhashstring";
 
 const express = require('express');
-const jwt = require("jsonwebtoken");
+const jwt     = require("jsonwebtoken");
+const crypto  = require("crypto");
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
+
+let all_users = [{user:"tamara",password:"5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"}]
+
+
+
+
+const check_login = (user,pwd) => {
+    let found_user = check_user(user);
+    let hash = crypto.createHash('sha256').update(pwd).digest('hex'); 
+    return (found_user && (found_user.password === hash))
+}
+
+const check_user = (user) =>{
+    let found_user = all_users.find((item)=>{
+        return item.user === user
+    });
+
+    return (found_user !== undefined ? found_user : false) 
+}
 
 
 const verifyToken = (req, res, next) => {
+
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
   
     if (!token) {
@@ -33,12 +56,30 @@ app.get('/protected_data',verifyToken,(req,res) => {
     res.status(200).send({msg:"this is a secret"})
 });
 
+app.post('/register',(req,res) => {
+    if(req.body.user && req.body.password){
+        let user = req.body.user;
+        let pwd  = req.body.password;
+        if(check_user(user) === false){
+            all_users.push({
+                user : user,
+                password : crypto.createHash('sha256').update(pwd).digest('hex')
+            })
+            res.status(200).send({msg:"user registered"})
+        }else{
+            res.status(400).send({msg:"User " + user + " already exists"})
+        }
+    }else{
+        res.status(400).send({msg:"Wrong data"})
+    }
+})
+
 app.post('/login', (req, res) => {
 
     if(req.body.user && req.body.password){
         let user = req.body.user;
         let pwd  = req.body.password;
-        if(user === "tamara" && pwd === "12345"){
+        if(check_login(user,pwd)){
             const token = jwt.sign(
                 { user },
                 SECRET,
